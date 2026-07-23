32fdae1f3887911d83dafa8ee8f4a61e1d3b78fb
Josh Persi <josh.persi@inspection.gc.ca>
2026-07-23 10:07:38 -0400

    refactor: supply `add_covariates()` a raster object
    - make `add_covariates()` take a raster object
    
    - abandon code to load a raster object

--- R/add-covariates.R
+++ R/add-covariates.R
@@ -1,25 +1,19 @@
 #' Add climate data covariates
-
+#'
+#' @param gbif_occurrence_data a dataframe of GBIF occurrence data
+#'
+#' @param covariate_file a terra raster object of covariate data
+#
 #' @export
 add_covariates <- function(
   gbif_occurrence_data = NULL,
-  emission_scenario = base::c("ssp370"),
-  time_period = base::c("2011.2040", "2041.2070")
+  covariate_raster = NULL
 ) {
   gbif_occurrence_data_vect <- terra::vect(
     gbif_occurrence_data,
     geom = base::c("decimalLongitude", "decimalLatitude"),
     "EPSG:4326"
   )
-
-  wracs_data_dir <- base::Sys.getenv("wracs_data_dir", unset = NA_character_)
-
-  covariate_raster_path <- fs::path(
-    wracs_data_dir,
-    glue::glue("raster.ensemble.{time_period}.{emission_scenario}.tif")
-  )
-
-  covariate_raster <- terra::rast(covariate_raster_path)
 
   df <- dplyr::mutate(
     gbif_occurrence_data,
